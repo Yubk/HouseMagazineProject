f932a880b51dc034b438ee39e44fe00aabf1ad6c
Yubk <Ybk@YubktekiMacBook-Pro.local>
2013-11-22 14:50:06 +0800

    Initial Commit

---
+++ HouseP/MYLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MYLAppDelegate.h
+//  HouseP
+//
+//  Created by Yubk on 13-11-22.
+//  Copyright (c) 2013年 Yubk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MYLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HouseP/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HouseP
+//
+//  Created by Yubk on 13-11-22.
+//  Copyright (c) 2013年 Yubk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MYLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MYLAppDelegate class]));
+    }
+}
